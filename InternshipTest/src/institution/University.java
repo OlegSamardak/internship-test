dede06f5d414eb51644d5e38c8d19d2b76eda0a6
unknown <os1998os@ukr.net>
2018-06-01 01:10:49 +0300

    added private modificator

--- InternshipTest/src/institution/University.java
+++ InternshipTest/src/institution/University.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class University {
     private String name;
-    List<Student> students = new ArrayList<>();
+    private List<Student> students = new ArrayList<>();
     public University(String name) {
         this.name = name;
     }
